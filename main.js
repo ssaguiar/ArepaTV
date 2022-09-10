b24e848aa970ed23b3a9cdc13f54bae6f90d7bc6
becerraenmanuel <BecerraCEnmanuelD@gmail.com>
2022-09-10 18:54:05 -0400

    update

--- main.js
+++ main.js
@@ -6,12 +6,10 @@ const worker = require('worker_threads');
 /*-------------------------------------------------------------------------------------------------*/
 
 const cors = new worker.Worker('./module/server/cors',{ env: worker.SHARE_ENV });
-cors.on('message',()=>{
-    const db = new worker.Worker('./module/server/db',{ env: worker.SHARE_ENV });
-    db.on('message',()=>{
-        const tm = new worker.Worker('./module/server/logs',{ env: worker.SHARE_ENV });
-        const http = new worker.Worker('./module/server/http',{ env: worker.SHARE_ENV });
-    });
+const db = new worker.Worker('./module/server/db',{ env: worker.SHARE_ENV });
+db.on('message',()=>{
+    const http = new worker.Worker('./module/server/http',{ env: worker.SHARE_ENV });
+    const tm = new worker.Worker('./module/server/logs',{ env: worker.SHARE_ENV });
 });
 
 /*-------------------------------------------------------------------------------------------------*/
